85c6087f91e55ca83839876eb3ab7c13e62b6abb
David McDonald <davemcdonald@live.co.uk>
2018-08-18 11:37:33 +0100

    urgh

--- slurm_cora.sh
+++ slurm_cora.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=coraJob_%A_%a.out
 #SBATCH --error=coraJob_%A_%a.err
 #SBATCH --array=0-2
-#SBATCH --time=24:00:00
+#SBATCH --time=10-00:00:00
 #SBATCH --ntasks=4
 #SBATCH --mem-per-cpu=16gb
 
@@ -19,8 +19,10 @@ module load apps/python2/2.7.11
 # module load apps/h5py/2.7.0-python-3.5.2
 # module load TensorFlow/1.8.0-foss-2018a-Python-3.6.3
 module load apps/tensorflow/1.3.1-python-2.7.11
- module load apps/keras/2.0.8-python-2.7.11
+module load apps/keras/2.0.8-python-2.7.11
 # module load matplotlib/2.1.1-iomkl-2018a-Python-3.6.3
+module load apps/cudnn/6.0
+-I${CUDNN_ROOT}/include/cudnn.h -L${CUDNN_ROOT}/lib64/libcudnn.so
 
 # echo done
 python embedding/hyperbolic_embedding.py  --dataset cora --dim 32 \
