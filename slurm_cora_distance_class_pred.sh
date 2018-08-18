433974753bfa1aa6f5a25e9fd4a2df042055d20f
David McDonald <davemcdonald@live.co.uk>
2018-08-18 14:11:04 +0100

    array job

--- slurm_cora_distance_class_pred.sh
+++ slurm_cora_distance_class_pred.sh
@@ -3,12 +3,12 @@
 #SBATCH --job-name=coraDistanceClassPred
 #SBATCH --output=coraDistanceClassPred_%A_%a.out
 #SBATCH --error=coraDistanceClassPred_%A_%a.err
-#SBATCH --array=0-2
+#SBATCH --array=0-26
 #SBATCH --time=3-00:00:00
 #SBATCH --ntasks=3
 #SBATCH --mem-per-cpu=16gb
 
-arr=(--no-attributes --multiply-attributes --jump-prob=0.05)
+arr=({--no-attributes,--multiply-attributes,--jump-prob=0.05}" "-r{1,3,5}" "-t{1,3,5})
 
 
 module purge; module load bluebear
@@ -26,4 +26,4 @@ module load apps/cudnn/6.0
 
 # echo done
 python embedding/hyperbolic_embedding.py  --dataset cora --dim 32 --data-directory /rds/homes/d/dxm237/data \
---no-load -r 1 -t 1 ${arr[${SLURM_ARRAY_TASK_ID}]}
+--no-load ${arr[${SLURM_ARRAY_TASK_ID}]}
