92f087567f7e04f0c2a0789123cc0a6b99aa596e
David McDonald <davemcdonald@live.co.uk>
2018-08-21 12:11:33 +0100

    citseer on K80

--- slurm_citeseer_distance_link_pred.sh
+++ slurm_citeseer_distance_link_pred.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
+#SBATCH --qos bbgpu
+#SBATCH --gres gpu:k20:1
 #SBATCH --job-name=citeseerDistanceLinkPred
 #SBATCH --output=citeseerDistanceLinkPred_%A_%a.out
 #SBATCH --error=citeseerDistanceLinkPred_%A_%a.err
 #SBATCH --array=0-26
-#SBATCH --time=10-00:00:00
-#SBATCH --ntasks=3
+#SBATCH --time=1-00:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task 5
 #SBATCH --mem=8gb
 
 
-arr=({--no-attributes,--multiply-attributes,--jump-prob=0.05}" "-r{1,3,5}" "-t{1,3,5})
+arr=(-r{5,3,1}" "-t{1,3,5}" "{--no-attributes,--multiply-attributes,--jump-prob=0.05})
 
 
 module purge; module load bluebear
-module load apps/cuda/8.0.44
-module load apps/cudnn/6.0
--I${CUDNN_ROOT}/include/cudnn.h -L${CUDNN_ROOT}/lib64/libcudnn.so
+# module load apps/cuda/8.0.44
+# module load apps/cudnn/6.0
+# -I${CUDNN_ROOT}/include/cudnn.h -L${CUDNN_ROOT}/lib64/libcudnn.so
 # module load bear-apps/2018a
 # module load Python/3.6.3-iomkl-2018a
 # module load apps/python2/2.7.11
@@ -28,7 +31,7 @@ module load apps/tensorflow/1.3.1-python-3.5.2-cuda-8.0.44
 # module load apps/tensorflow/1.3.1-python-2.7.11-cuda-8.0.44
 # module load apps/keras/2.0.8-python-2.7.11
 module load apps/keras/2.0.8-python-3.5.2-cuda-8.0.44
-# echo done
 
+echo starting, ${arr[${SLURM_ARRAY_TASK_ID}]}
 python embedding/hyperbolic_embedding.py  --dataset citeseer --dim 128 --data-directory /rds/homes/d/dxm237/data \
---no-load ${arr[${SLURM_ARRAY_TASK_ID}]} --evaluate-link-prediction
+--no-load ${arr[${SLURM_ARRAY_TASK_ID}]} --evaluate-link-prediction --workers 4
