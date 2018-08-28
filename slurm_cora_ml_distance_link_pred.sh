c6adfab44029a9d55b70ba894381ac352c769e0a
David McDonald <davemcdonald@live.co.uk>
2018-08-28 19:00:19 +0100

    fe

--- slurm_cora_ml_distance_link_pred.sh
+++ slurm_cora_ml_distance_link_pred.sh
@@ -10,28 +10,12 @@
 #SBATCH --ntasks=1
 #SBATCH --mem=8gb
 
-
-# arr=(-r{5,3,1}" "-t{1,3,5}" "{--no-attributes,--multiply-attributes,--jump-prob=0.05})
-# arr=(-r=1" "-t=1" "--dim=2" "{--no-non-edges,--add-non-edges}" "{--no-attributes,--multiply-attributes,--jump-prob=0.05})
-
 arr=({--softmax,-r=3" "-t={1,3}}" "--dim={2,32,128}" --evaluate-link-prediction "{--no-attributes,--multiply-attributes,--jump-prob=0.05})
 
 module purge; module load bluebear
-# module load apps/cuda/8.0.44
-# module load apps/cudnn/6.0
-# -I${CUDNN_ROOT}/include/cudnn.h -L${CUDNN_ROOT}/lib64/libcudnn.so
-# module load bear-apps/2018a
-# module load Python/3.6.3-iomkl-2018a
-# module load apps/python2/2.7.11
-module load apps/python3/3.5.2
-# module load apps/scikit-learn/0.19.0-python-3.5.2
-# module load apps/h5py/2.7.0-python-3.5.2
-# module load TensorFlow/1.8.0-foss-2018a-Python-3.6.3
-# module load apps/tensorflow/1.3.1-python-2.7.11
+module load apps/python3/3.5.23
 module load apps/tensorflow/1.3.1-python-3.5.2-cuda-8.0.44
-# module load apps/tensorflow/1.3.1-python-2.7.11-cuda-8.0.44
-# module load apps/keras/2.0.8-python-2.7.11
 module load apps/keras/2.0.8-python-3.5.2-cuda-8.0.44
 
 echo starting, ${arr[${SLURM_ARRAY_TASK_ID}]}
-python embedding/hyperbolic_embedding.py  --dataset cora_ml --data-directory /rds/homes/d/dxm237/data --no-load ${arr[${SLURM_ARRAY_TASK_ID}]} 
+python embedding/hyperbolic_embedding.py --dataset cora_ml --data-directory /rds/homes/d/dxm237/data --no-load ${arr[${SLURM_ARRAY_TASK_ID}]} 
