ea2a4a1690c26d4821cc733a29620886090af613
David McDonald <davemcdonald@live.co.uk>
2018-08-29 15:17:09 +0100

    new slurm script

--- slurm_cora_ml_distance_link_pred.sh
+++ slurm_cora_ml_distance_link_pred.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 #SBATCH --qos bbgpu
-#SBATCH --gres gpu:p100:1
+#SBATCH --gres gpu:k20:1
 #SBATCH --job-name=coraMLDistanceLinkPred
 #SBATCH --output=coraMLDistanceLinkPred_%A_%a.out
 #SBATCH --error=coraMLDistanceLinkPred_%A_%a.err
-#SBATCH --array=0-26
+#SBATCH --array=0-59
 #SBATCH --time=1-00:00:00
 #SBATCH --ntasks=1
 #SBATCH --mem=8gb
 
-arr=({--softmax,-r=3" "-t={1,3}}" "--dim={2,32,128}" --evaluate-link-prediction "{--no-attributes,--multiply-attributes,--jump-prob=0.05})
+arr=(r={5,3}" "-t={1,3}" "--dim={2,3,5,32,128}" --evaluate-link-prediction "{--no-attributes,--multiply-attributes,--jump-prob=0.05})
 
 module purge; module load bluebear
-module load apps/python3/3.5.23
+module load apps/python3/3.5.2
 module load apps/tensorflow/1.3.1-python-3.5.2-cuda-8.0.44
 module load apps/keras/2.0.8-python-3.5.2-cuda-8.0.44
 
