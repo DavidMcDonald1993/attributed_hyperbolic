bc82a4b5ca70d07206ed600c0ec4bc0760b361b5
David McDonald <davemcdonald@live.co.uk>
2018-08-30 09:02:54 +0100

    change in project dir

--- slurm_cora_ml_distance_link_pred.sh
+++ slurm_cora_ml_distance_link_pred.sh
@@ -10,12 +10,13 @@
 #SBATCH --ntasks=1
 #SBATCH --mem=8gb
 
-arr=(r={5,3}" "-t={1,3}" "--dim={2,3,5,32,128}" --evaluate-link-prediction "{--no-attributes,--multiply-attributes,--jump-prob=0.05})
+PROJECT_DIR=/rds/projects/2018/hesz01/attributed_hyperbolic
+ARR=(r={5,3}" "-t={1,3}" "--dim={2,3,5,32,128}" --evaluate-link-prediction "{--no-attributes,--multiply-attributes,--jump-prob=0.05})
 
 module purge; module load bluebear
 module load apps/python3/3.5.2
 module load apps/tensorflow/1.3.1-python-3.5.2-cuda-8.0.44
 module load apps/keras/2.0.8-python-3.5.2-cuda-8.0.44
 
-echo starting, ${arr[${SLURM_ARRAY_TASK_ID}]}
-python embedding/hyperbolic_embedding.py --dataset cora_ml --data-directory /rds/homes/d/dxm237/data --no-load ${arr[${SLURM_ARRAY_TASK_ID}]} 
+echo starting, ${ARR[${SLURM_ARRAY_TASK_ID}]}
+python embedding/hyperbolic_embedding.py --dataset cora_ml --data-directory ${PROJECT_DIR}/data --no-load ${ARR[${SLURM_ARRAY_TASK_ID}]} 
