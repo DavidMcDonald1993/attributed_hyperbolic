2868fd7b13561b08150a69344feb7842911630fa
David McDonald <davemcdonald@live.co.uk>
2018-09-11 08:07:12 +0100

    citeseer experiments on k20

--- slurm_citeseer_distance_link_pred.sh
+++ slurm_citeseer_distance_link_pred.sh
@@ -12,26 +12,15 @@
 #SBATCH --mem=8gb
 
 
-arr=(-r{5,3,1}" "-t{1,3,5}" "{--no-attributes,--multiply-attributes,--jump-prob=0.05})
 
+PROJECT_DIR=/rds/projects/2018/hesz01/attributed_hyperbolic
+# ARR=({--softmax,-r={5,3}" "-t={1,3}}" "--dim={2,3,5,32,128}" --evaluate-link-prediction "{--no-attributes,--multiply-attributes,--jump-prob=0.05})
+ARR=(--seed={0..29}" "--softmax" "--dim=3" --evaluate-link-prediction "--no-attributes)
 
 module purge; module load bluebear
-# module load apps/cuda/8.0.44
-# module load apps/cudnn/6.0
-# -I${CUDNN_ROOT}/include/cudnn.h -L${CUDNN_ROOT}/lib64/libcudnn.so
-# module load bear-apps/2018a
-# module load Python/3.6.3-iomkl-2018a
-# module load apps/python2/2.7.11
 module load apps/python3/3.5.2
-# module load apps/scikit-learn/0.19.0-python-3.5.2
-# module load apps/h5py/2.7.0-python-3.5.2
-# module load TensorFlow/1.8.0-foss-2018a-Python-3.6.3
-# module load apps/tensorflow/1.3.1-python-2.7.11
 module load apps/tensorflow/1.3.1-python-3.5.2-cuda-8.0.44
-# module load apps/tensorflow/1.3.1-python-2.7.11-cuda-8.0.44
-# module load apps/keras/2.0.8-python-2.7.11
 module load apps/keras/2.0.8-python-3.5.2-cuda-8.0.44
 
-echo starting, ${arr[${SLURM_ARRAY_TASK_ID}]}
-python embedding/hyperbolic_embedding.py  --dataset citeseer --dim 128 --data-directory /rds/homes/d/dxm237/data \
---no-load ${arr[${SLURM_ARRAY_TASK_ID}]} --evaluate-link-prediction --workers 4
+echo starting, ${ARR[${SLURM_ARRAY_TASK_ID}]}
+python embedding/hyperbolic_embedding.py --dataset citeseer --data-directory ${PROJECT_DIR}/data --no-load ${ARR[${SLURM_ARRAY_TASK_ID}]} 
