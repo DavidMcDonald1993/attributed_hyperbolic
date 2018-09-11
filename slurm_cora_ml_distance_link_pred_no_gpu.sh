3b5570b9ba3e362a80fea2bdf091a43644d50924
David McDonald <davemcdonald@live.co.uk>
2018-09-11 15:39:56 +0100

    cora_ml no gpu

---
+++ slurm_cora_ml_distance_link_pred_no_gpu.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --job-name=noGPUCoraMLDistanceLinkPred
+#SBATCH --output=noGPUCoraMLDistanceLinkPred_%A_%a.out
+#SBATCH --error=noGPUCoraMLDistanceLinkPred_%A_%a.err
+#SBATCH --array=0-29
+#SBATCH --time=3-00:00:00
+#SBATCH --ntasks=1
+#SBATCH --mem=8gb
+
+
+
+PROJECT_DIR=/rds/projects/2018/hesz01/attributed_hyperbolic
+# ARR=({--softmax,-r={5,3}" "-t={1,3}}" "--dim={2,3,5,32,128}" --evaluate-link-prediction "{--no-attributes,--multiply-attributes,--jump-prob=0.05})
+ARR=(--seed={0..29}" "--softmax" "--dim=3" --evaluate-link-prediction "--no-attributes)
+
+module purge; module load bluebear
+module load apps/cuda/8.0.44
+module load apps/cudnn/6.0
+-I${CUDNN_ROOT}/include/cudnn.h -L${CUDNN_ROOT}/lib64/libcudnn.so
+module load apps/python3/3.5.2
+module load apps/tensorflow/1.3.1-python-3.5.2
+module load apps/keras/2.0.8-python-3.5.2
+
+echo starting, ${ARR[${SLURM_ARRAY_TASK_ID}]}
+python embedding/hyperbolic_embedding.py --dataset cora_ml --data-directory ${PROJECT_DIR}/data --no-load ${ARR[${SLURM_ARRAY_TASK_ID}]} 
