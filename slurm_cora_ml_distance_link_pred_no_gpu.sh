88b5a10872eabb67ba7a0e06a2e39997fecdbfb3
David McDonald <davemcdonald@live.co.uk>
2018-09-19 10:10:39 +0100

    type fix

--- slurm_cora_ml_distance_link_pred_no_gpu.sh
+++ slurm_cora_ml_distance_link_pred_no_gpu.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-#SBATCH --job-name=noGPUCoraMLDistanceLinkPred5
-#SBATCH --output=noGPUCoraMLDistanceLinkPred5_%A_%a.out
-#SBATCH --error=noGPUCoraMLDistanceLinkPred5_%A_%a.err
-#SBATCH --array=0-119
+#SBATCH --job-name=noGPUCoraMLDistanceLinkPred
+#SBATCH --output=noGPUCoraMLDistanceLinkPred_%A_%a.out
+#SBATCH --error=noGPUCoraMLDistanceLinkPred_%A_%a.err
+#SBATCH --array=0-179
 #SBATCH --time=3-00:00:00
 #SBATCH --ntasks=1
 #SBATCH --mem=8G
 
 
-
 PROJECT_DIR=/rds/projects/2018/hesz01/attributed_hyperbolic
-# ARR=({--softmax,-r={5,3}" "-t={1,3}}" "--dim={2,3,5,32,128}" --evaluate-link-prediction "{--no-attributes,--multiply-attributes,--jump-prob=0.05})
-ARR=(--dim={3,5}" "--seed={0..29}" "--softmax" "--evaluate-link-prediction" "--{no-attributes,jump-prob=0.05})
+ARR=(--dim={3,5}" "--seed={0..29}" "--softmax" "--evaluate-link-prediction" "--{no-attributes,jump-prob={0.05,0.1}})
 
 module purge; module load bluebear
 module load apps/cuda/8.0.44
