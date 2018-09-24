2966ad18dc200569c43b34035c2a4b79ea65ae08
David McDonald <davemcdonald@live.co.uk>
2018-09-24 13:16:10 +0100

    context size experiments

--- slurm_cora_ml_distance_link_pred_no_gpu.sh
+++ slurm_cora_ml_distance_link_pred_no_gpu.sh
@@ -22,8 +22,8 @@ module load apps/python3/3.5.2
 module load apps/tensorflow/1.3.1-python-3.5.2
 module load apps/keras/2.0.8-python-3.5.2
 
-echo "staring dataset=cora_ml ct3 "${ARR[${SLURM_ARRAY_TASK_ID}]}
+echo "staring dataset=cora_ml ct1 "${ARR[${SLURM_ARRAY_TASK_ID}]}
 python embedding/hyperbolic_embedding.py --dataset cora_ml \
 --data-directory ${PROJECT_DIR}/data --lr .3 --no-load ${ARR[${SLURM_ARRAY_TASK_ID}]} \
---context-size 3 --model models_ct3 --plot plots_ct3 --logs logs_ct3 --test-results test_results_ct3 
-echo "completed dataset=cora_ml ct3"${ARR[${SLURM_ARRAY_TASK_ID}]}
+--context-size 1 
+echo "completed dataset=cora_ml ct1"${ARR[${SLURM_ARRAY_TASK_ID}]}
