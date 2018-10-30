87870f43472c1aa3b6e07b3d9a37ce200bce1061
David McDonald <davemcdonald@live.co.uk>
2018-10-30 21:37:29 +0000

    lets try again

---
+++ make_directory_structure.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# datasets=({cora_ml,citeseer,pubmed,cora})
+# dim=({002,003,005,010,025,050,100})
+# seed={000..29}
+# att={no_attributes,multiply_attributes,jump_prob={0.05,0.1,0.2}}
+
+
+rm -r {models,plots,test_results,walks,logs}
+
+
+mkdir -p {models,plots,logs}/{cora_ml,citeseer,pubmed,cora}/\
+dim={002,003,005,010,020,050,100}/seed={000..29}/all_components/eval_lp/no_non_edges/\
+{no_attributes,multiply_attributes,jump_prob={0.05,0.1,0.2}}
+
+mkdir -p walks/{cora_ml,citeseer,pubmed,cora}/\
+seed={000..29}/all_components/eval_lp/no_non_edges/\
+{no_attributes,multiply_attributes,jump_prob={0.05,0.1,0.2}}
+
+mkdir -p test_results/{cora_ml,citeseer,pubmed,cora}/\
+dim={002,003,005,010,020,050,100}/all_components/eval_lp/no_non_edges/\
+{no_attributes,multiply_attributes,jump_prob={0.05,0.1,0.2}}
+
+touch test_results/{cora_ml,citeseer,pubmed,cora}/\
+dim={002,003,005,010,020,050,100}/all_components/eval_lp/no_non_edges/\
+{no_attributes,multiply_attributes,jump_prob={0.05,0.1,0.2}}/test_results.lock
