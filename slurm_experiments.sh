5337556ff088cc3127044955ae9a4bf18f89066c
David McDonald <davemcdonald@live.co.uk>
2019-02-11 11:49:46 +0000

    fee

--- slurm_experiments.sh
+++ slurm_experiments.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=noGPUexp
 #SBATCH --output=noGPUexp_%A_%a.out
 #SBATCH --error=noGPUexp_%A_%a.err
-#SBATCH --array=0-1679
+#SBATCH --array=0-419
 #SBATCH --time=1-00:00:00
 #SBATCH --ntasks=1
 #SBATCH --mem=32G
@@ -13,7 +13,7 @@ DATA_DIR="/rds/homes/d/dxm237/data"
 
 # ARR=(--dataset={cora_ml,citeseer,pubmed}" "--seed={0..29}" "--softmax" "--dim={5,10,25,50}" --evaluate-"{link,class}"-prediction "--{no-attributes,jump-prob={.05,.1,.2}})
 # ARR=(--dataset={cora_ml,citeseer}" "--seed={0..29}" "--softmax" "--dim={5,10,25,50}" --evaluate-"{link,class}"-prediction "--{no-attributes,jump-prob={.05,.1,.2,.5,.8,1.}})
-ARR=(--dataset=pubmed" "--seed={0..29}" "--softmax" "--dim={5,10,25,50}" --evaluate-"{link,class}"-prediction "--{no-attributes,jump-prob={.05,.1,.2,.5,.8,1.}})
+ARR=(--dataset=pubmed" "--seed={0..29}" "--softmax" "--dim=5" --evaluate-"{link,class}"-prediction "--{no-attributes,jump-prob={.05,.1,.2,.5,.8,1.}})
 
 
 module purge; module load bluebear
