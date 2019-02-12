20b9fdbe519c4c202275d033faec2e9cc05f1ea6
David McDonald <davemcdonald@live.co.uk>
2019-02-12 11:49:25 +0000

    plot degree dists added

---
+++ plot_degree_dists.py
@@ -0,0 +1,51 @@
+import argparse
+
+import numpy as np
+
+from embedding.data_utils import load_g2g_datasets, load_ppi
+from embedding.visualise import plot_degree_dist
+
+import matplotlib.pyplot as plt
+
+def parse_args():
+	'''
+	parse args from the command line
+	'''
+	parser = argparse.ArgumentParser(description="Plot degree dists script")
+
+	parser.add_argument("--data-directory", dest="data_directory", type=str, default="/data/",
+		help="The directory containing data files (default is '/data/').")
+
+	parser.add_argument('--directed', action="store_true", help='flag to train on directed graph')
+
+
+	parser.add_argument('--only-lcc', action="store_true", help='flag to train on only lcc')
+
+	args = parser.parse_args()
+	return args
+
+def main():
+
+	args = parse_args()
+
+	datasets = ["cora_ml", "citeseer", "pubmed", "ppi"]
+
+	f, a = plt.subplots(nrows=1, ncols=len(datasets), figsize=(5*len(datasets), 5), dpi= 80)
+
+	for i, dataset in enumerate(datasets):
+
+		if dataset in ["cora_ml", "citeseer", "pubmed"]:
+			load = load_g2g_datasets
+		else:
+			load = load_ppi
+
+		graph, _, _ = load(dataset, args)
+		plot_degree_dist(dataset, graph, a[i])
+
+	plt.savefig("degree_dist_plot.png")
+	plt.close()
+
+
+
+if __name__ == "__main__":
+	main()
---
+++ slurm_degree_dist.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name=degreeDist
+#SBATCH --output=noGPUexp.out
+#SBATCH --error=noGPUexp_%A_%a.err
+#SBATCH --time=1-00:00:00
+#SBATCH --ntasks=1
+#SBATCH --mem=64G
+# SBATCH --mail-type ALL
+
+DATA_DIR="/rds/homes/d/dxm237/data"
+
+module purge; module load bluebear
+module load apps/python3/3.5.2
+python plot_degree_dists.py --data-directory ${DATA_DIR} 
