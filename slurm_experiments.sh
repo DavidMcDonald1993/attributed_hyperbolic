0c21b8eac18f062b723950b58c5afbcaab3b9849
David McDonald <davemcdonald@live.co.uk>
2019-02-13 09:27:26 +0000

    removed old code

--- slurm_experiments.sh
+++ slurm_experiments.sh
@@ -3,18 +3,18 @@
 #SBATCH --job-name=noGPUexp
 #SBATCH --output=noGPUexp_%A_%a.out
 #SBATCH --error=noGPUexp_%A_%a.err
-#SBATCH --array=0-559
+#SBATCH --array=0-419
 #SBATCH --time=1-00:00:00
 #SBATCH --ntasks=1
-#SBATCH --mem=100G
+#SBATCH --mem=10G
 # SBATCH --mail-type ALL
 
 DATA_DIR="/rds/homes/d/dxm237/data"
 
 # ARR=(--dataset={cora_ml,citeseer,pubmed}" "--seed={0..29}" "--softmax" "--dim={5,10,25,50}" --evaluate-"{link,class}"-prediction "--{no-attributes,jump-prob={.05,.1,.2}})
 # ARR=(--dataset={cora_ml,citeseer}" "--seed={0..29}" "--softmax" "--dim={5,10,25,50}" --evaluate-"{link,class}"-prediction "--{no-attributes,jump-prob={.05,.1,.2,.5,.8,1.}})
-ARR=(--dataset=pubmed" "--seed={0..9}" "--softmax" "--dim={5,10,25,50}" --evaluate-"{link,class}"-prediction "--{no-attributes,jump-prob={.05,.1,.2,.5,.8,1.}})
-# ARR=(--dataset=ppi" "--seed={0..29}" "--softmax" "--dim=5" --evaluate-"{link,class}"-prediction "--{no-attributes,jump-prob={.05,.1,.2,.5,.8,1.}})
+# ARR=(--dataset=pubmed" "--seed={0..9}" "--softmax" "--dim={5,10,25,50}" --evaluate-"{link,class}"-prediction "--{no-attributes,jump-prob={.05,.1,.2,.5,.8,1.}})
+ARR=(--dataset=ppi" --only-lcc "--seed={0..29}" "--softmax" "--dim=5" --evaluate-"{link,class}"-prediction "--{no-attributes,jump-prob={.05,.1,.2,.5,.8,1.}})
 
 module purge; module load bluebear
 module load apps/python3/3.5.2
